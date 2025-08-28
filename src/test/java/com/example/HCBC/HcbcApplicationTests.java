79106c9c398c0ae7dffa2109b6e18b7b35b01660
hej090224 <fc49854985@gmail.com>
2025-08-28 17:53:09 +0900

    초기 생성

---
+++ src/test/java/com/example/HCBC/HcbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HCBC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HcbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
